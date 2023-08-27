0bd83a722addb72d7d6001cd9975c51576869a93
madisonkell <106936494+madisonkell@users.noreply.github.com>
2023-08-27 16:25:02 -0400

    First iteration of basic math functions added

---
+++ basicmath.py
@@ -0,0 +1,21 @@
+#This file contains the ability to do basic the four basic math functions for pykalk
+
+#Add two numbers and return the result
+def doAdd(num1, num2):
+    answer = num1 + num2
+    return answer
+
+#Subtract two numbers and return the result
+def doSub(num1, num2):
+    answer = num1 - num2
+    return answer
+
+#Multiply two numbers and return the result
+def doMul(num1, num2):
+    answer = num1 * num2
+    return answer
+
+#Divide two numbers and return the result
+def doDiv(num1, num2):
+    answer = num1 / num2
+    return answer
