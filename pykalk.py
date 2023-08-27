f0e57f8ab031c291859547ebb9c08b9738e5693f
madisonkell <106936494+madisonkell@users.noreply.github.com>
2023-08-27 16:20:26 -0400

    printing numeric answer version

--- pykalk.py
+++ pykalk.py
@@ -1,1 +1,14 @@
-print ("This is the start of pykalk!")
+#This is a Python comment
+#There are no { } blocks in Python.  Instead you space over
+#4 spaces when you need to indicate a body.  Notice that I am
+#defining a function called printout.  The definition ends with a :
+#indicating that a code suite (you can think of it as a block) is
+#coming up next.  I space each line in the suite over 4 spaces -- do
+#not use a tab!  That can mess things up.  You have to be consistent
+#in the number of spaces.  4 is what Guido von Rossum, Python's inventor
+#uses as the de facto standard.
+
+def printAnswer(s):
+    print(s)
+
+printAnswer("5 + 7 = 12")
